fcbd5b7ac4bdfc67e219dd2a7036b433ff33f9dd
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-18 19:09:39 +0200

    01-navbar-hero-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
 
@@ -6,6 +7,7 @@ const App = () => {
   return (
       <main>
         <Navbar/>
+        <Hero/>
       </main>
   )
 }
