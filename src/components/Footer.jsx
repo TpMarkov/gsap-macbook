f27dded66b3a8e8daf8d6ff939bedf9ea0737d5f
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-22 12:47:28 +0200

    add Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,24 +8,23 @@ const Footer = () => {
         More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966
       </p>
       <img src="/logo.svg" alt="logo"/>
-
-      <hr/>
-
-      <div className={"link"}>
-        <p>Copyright © 2025 Apple Inc. All rights reserved</p>
-        <ul>
-          {footerLinks.map(({label, link}) => (
-              <li key={label}>
-                <a href={link}>
-                  {label}
-                </a>
-              </li>
-          ))}
-        </ul>
-      </div>
     </div>
 
+    <hr/>
 
+    <div className={"link"}>
+      <p>Copyright © 2025 Apple Inc. All rights reserved</p>
+
+      <ul>
+        {footerLinks.map(({label, link}) => (
+            <li key={label}>
+              <a href={link}>
+                {label}
+              </a>
+            </li>
+        ))}
+      </ul>
+    </div>
   </footer>;
 };
 export default Footer;
